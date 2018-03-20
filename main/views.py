47b7215989e5ee9dbabac0f44514bd4f4ecfd0f3
Ankit Goel <ankitgoel616@gmail.com>
2018-03-20 09:59:23 +0000

    Factor out api_key into environment variables

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+import os
+
+api_key = os.environ['API_KEY']
+api_secret = os.environ['API_SECRET']
 
 # Create your views here.
 
@@ -6,7 +10,7 @@ def index(request):
     return render(request, "main/index.html")
 
 def login(request):
-    return redirect('https://kite.trade/connect/login?v=3&api_key=hw1kqautubteevkq')
+    return redirect('https://kite.trade/connect/login?v=3&api_key=%s' % api_key)
 
 def request_token(request):
     token = request.params['request_token']
