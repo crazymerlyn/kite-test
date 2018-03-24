331b28a26627974a91be7e3ec80d2ffc8ca06100
Ankit Goel <ankitgoel616@gmail.com>
2018-03-24 03:06:20 +0000

    Minor changes

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
         url('^login', views.login, name='login'),
-        url('^request_token', views.request_token, name='login'),
+        url('^request_token', views.request_token, name='request_token'),
         url('', views.index, name='index'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from kiteconnect import KiteConnect
 import os
 
 api_key = os.environ['API_KEY']
@@ -7,11 +8,20 @@ api_secret = os.environ['API_SECRET']
 # Create your views here.
 
 def index(request):
-    return render(request, "main/index.html")
+    ctx = {}
+    ctx['access_token'] = request.session.get('access_token', '')
+    if ctx['access_token']:
+        kite = KiteConnect(api_key=api_key)
+        kite.set_access_token(ctx['access_token'])
+        ctx['orders'] = kite.instruments()[:10]
+    return render(request, "main/index.html", ctx)
 
 def login(request):
     return redirect('https://kite.trade/connect/login?v=3&api_key=%s' % api_key)
 
 def request_token(request):
-    token = request.params['request_token']
-    return render(request, 'main/final.html', {'token': token})
+    token = request.GET.get('request_token')
+    kite = KiteConnect(api_key=api_key)
+    data = kite.generate_session(token, api_secret)
+    request.session['access_token'] = data['access_token']
+    return redirect(index)
